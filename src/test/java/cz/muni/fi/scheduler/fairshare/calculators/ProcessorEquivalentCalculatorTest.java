47aa83532b995c59e6cdd4754a285723296bb1dc
Andras Urge <andris@mail.muni.cz>
2016-10-16 13:18:59 +0200

    Tests for ProcessorEquivalentCalculator

--- src/test/java/cz/muni/fi/scheduler/fairshare/calculators/ProcessorEquivalentCalculatorTest.java
+++ src/test/java/cz/muni/fi/scheduler/fairshare/calculators/ProcessorEquivalentCalculatorTest.java
@@ -5,10 +5,17 @@
  */
 package cz.muni.fi.scheduler.fairshare.calculators;
 
+import cz.muni.fi.scheduler.elementpools.IHostPool;
+import cz.muni.fi.scheduler.elementpools.IVmPool;
+import cz.muni.fi.scheduler.resources.HostElement;
 import cz.muni.fi.scheduler.resources.VmElement;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -16,22 +23,61 @@ import static org.junit.Assert.*;
  */
 public class ProcessorEquivalentCalculatorTest {
     
-    public ProcessorEquivalentCalculatorTest() {
-    }
+    private ProcessorEquivalentCalculator calculator; 
+    private IVmPool vmPool; 
+    private IHostPool hostPool;     
     
     @Before
     public void setUp() {
+        vmPool = mock(IVmPool.class);
+        hostPool = mock(IHostPool.class);
+        when(hostPool.getHosts()).thenReturn(getHosts());
+        calculator = new ProcessorEquivalentCalculator(vmPool, hostPool, false);
     }
 
+    @Test(expected = NullPointerException.class) 
+    public void testGetPenalty_Null_ThrowsException() {   
+        calculator.getPenalty(null);
+    }
+    
     @Test
-    public void testGetPenalty() {
-        System.out.println("getPenalty");
-        VmElement vm = null;
-        ProcessorEquivalentCalculator instance = null;
-        float expResult = 0.0F;
-        float result = instance.getPenalty(vm);
-        assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
+    public void testGetPenalty_WithHighCpuReq() {
+        VmElement vm = new VmElement();
+        vm.setCpu(5f);
+        vm.setMemory(256);
+        
+        float result = calculator.getPenalty(vm);
+       
+        double delta = 0.0001;               
+        assertEquals("Penalty is not equal to CPU.", 5, result, delta);
+    }
+    
+    @Test
+    public void testGetPenalty_WithHighMemoryReq() {
+        VmElement vm = new VmElement();
+        vm.setCpu(1f);
+        vm.setMemory(1024);
+        
+        float result = calculator.getPenalty(vm);
+       
+        double delta = 0.0001;               
+        assertEquals("Penalty is not equal to CPU.", 4, result, delta);
+    }
+    
+    private List<HostElement> getHosts() {
+        List<HostElement> hosts = new ArrayList<>();
+                
+        hosts.add(createHostElement(2, 512));
+        hosts.add(createHostElement(4, 1024));
+        
+        return hosts;
+    }
+    
+    private HostElement createHostElement(float maxCpu, int maxMemory) {
+        HostElement host = new HostElement();
+        host.setMax_cpu(maxCpu);
+        host.setMax_mem(maxMemory);
+        return host;
     }
     
 }
