ad714ab894161778c6ff30dd92396b1663be56fb
Andras <andris@mail.muni.cz>
2016-11-02 12:37:13 +0100

    Dividing BeenConfig to multiple onfiguration files

---
+++ src/main/java/cz/muni/fi/config/PoolConfig.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.config;
+
+import cz.muni.fi.exceptions.LoadingFailedException;
+import cz.muni.fi.one.pools.AclElementPool;
+import cz.muni.fi.one.pools.ClusterElementPool;
+import cz.muni.fi.one.pools.DatastoreElementPool;
+import cz.muni.fi.one.pools.HostElementPool;
+import cz.muni.fi.one.pools.UserElementPool;
+import cz.muni.fi.one.pools.VmElementPool;
+import cz.muni.fi.scheduler.PropertiesConfig;
+import cz.muni.fi.scheduler.elementpools.IAclPool;
+import cz.muni.fi.scheduler.elementpools.IClusterPool;
+import cz.muni.fi.scheduler.elementpools.IDatastorePool;
+import cz.muni.fi.scheduler.elementpools.IHostPool;
+import cz.muni.fi.scheduler.elementpools.IUserPool;
+import cz.muni.fi.scheduler.elementpools.IVmPool;
+import cz.muni.fi.xml.pools.ClusterXmlPool;
+import cz.muni.fi.xml.pools.DatastoreXmlPool;
+import cz.muni.fi.xml.pools.HostXmlPool;
+import cz.muni.fi.xml.pools.UserXmlPool;
+import cz.muni.fi.xml.pools.VmXmlPool;
+import java.io.IOException;
+import org.opennebula.client.Client;
+import org.opennebula.client.ClientConfigurationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Andras Urge
+ */
+@Configuration
+public class PoolConfig {
+    
+    private PropertiesConfig properties;
+    
+    public PoolConfig() throws IOException {
+        properties = new PropertiesConfig("configuration.properties");
+    }
+    
+    @Bean 
+    public IAclPool aclPool() throws LoadingFailedException { 
+        if (properties.getBoolean("useXml")) {
+            return null;
+        }
+        return new AclElementPool(client());               
+    }
+    
+    @Bean 
+    public IVmPool vmPool() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            try {
+                return new VmXmlPool(properties.getString("vmpoolpath"));
+            } catch (IOException ex) {
+                throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+            }
+        } else {
+            return new VmElementPool(client());
+        }        
+    }
+    
+    @Bean 
+    public IUserPool userPool() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            try {
+                return new UserXmlPool(properties.getString("userpoolpath"));
+            } catch (IOException ex) {
+                throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+            }
+        } else {
+            return new UserElementPool(client());
+        }        
+    }
+    
+    @Bean 
+    public IHostPool hostPool() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            try {
+                return new HostXmlPool(properties.getString("hostpoolpath"));
+            } catch (IOException ex) {
+                throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+            }
+        } else {
+            return new HostElementPool(client());
+        }        
+    }
+    
+    @Bean 
+    public IClusterPool clusterPool() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            try {
+                return new ClusterXmlPool(properties.getString("clusterpoolpath"));
+            } catch (IOException ex) {
+                throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+            }
+        } else {
+            return new ClusterElementPool(client());
+        }        
+    }
+    
+    @Bean 
+    public IDatastorePool datastorePool() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            try {
+                return new DatastoreXmlPool(properties.getString("datastorepoolpath"));
+            } catch (IOException ex) {
+                throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+            }
+        } else {
+            return new DatastoreElementPool(client());
+        }        
+    }
+    
+    @Bean 
+    public Client client() throws LoadingFailedException{
+        if (properties.getBoolean("useXml")) {
+            return null;
+        }
+        try {
+            return new Client(properties.getString("secret"), properties.getString("endpoint"));
+        } catch (ClientConfigurationException ex) {
+            throw new LoadingFailedException(ex.getMessage(), ex.getCause());
+        }
+    }   
+}
---
+++ src/main/java/cz/muni/fi/config/SchedulerConfig.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.config;
+
+import cz.muni.fi.authorization.AuthorizationManager;
+import cz.muni.fi.authorization.AuthorizationManagerXml;
+import cz.muni.fi.authorization.IAuthorizationManager;
+import cz.muni.fi.exceptions.LoadingFailedException;
+import cz.muni.fi.scheduler.PropertiesConfig;
+import cz.muni.fi.scheduler.Scheduler;
+import cz.muni.fi.scheduler.policies.datastores.IStoragePolicy;
+import cz.muni.fi.scheduler.policies.datastores.StoragePacking;
+import cz.muni.fi.scheduler.policies.datastores.StorageStriping;
+import cz.muni.fi.scheduler.policies.hosts.IPlacementPolicy;
+import cz.muni.fi.scheduler.policies.hosts.LoadAware;
+import cz.muni.fi.scheduler.policies.hosts.Packing;
+import cz.muni.fi.scheduler.policies.hosts.Striping;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ *
+ * @author Andras Urge
+ */
+@Configuration
+@Import({PoolConfig.class, FilterConfig.class, FairshareConfig.class})
+public class SchedulerConfig {
+    
+    private PropertiesConfig properties;    
+    
+    @Autowired PoolConfig poolConfig;
+    @Autowired FilterConfig filterConfig;
+    @Autowired FairshareConfig fairshareConfig; 
+    
+    private static final String HOST_LOAD_AWARE_POLICY = "LoadAware";
+    private static final String HOST_PACKING_POLICY = "Packing";  
+    private static final String HOST_STRIPING_POLICY = "Striping";
+    
+    private static final String DS_PACKING_POLICY = "StoragePacking";  
+    private static final String DS_STRIPING_POLICY = "StorageStriping";
+
+    public SchedulerConfig() throws IOException {
+        properties = new PropertiesConfig("configuration.properties");
+    }
+    
+    @Bean 
+    public Scheduler scheduler() throws LoadingFailedException {
+        return new Scheduler(authorizationManager(), 
+                             poolConfig.hostPool(),
+                             poolConfig.vmPool(), 
+                             poolConfig.datastorePool(), 
+                             filterConfig.schedulingHostFilter(), 
+                             filterConfig.schedulingDsFilter(), 
+                             placementPolicy(), 
+                             storagePolicy(), 
+                             fairshareConfig.fairshareOrderer(), 
+                             properties.getInt("numberofqueues"), 
+                             properties.getBoolean("preferHostFit"));
+    }
+    
+    @Bean
+    public IPlacementPolicy placementPolicy() throws LoadingFailedException {
+        switch(properties.getString("hostPolicies")) {
+            case HOST_LOAD_AWARE_POLICY:
+                return new LoadAware();
+            case HOST_PACKING_POLICY:
+                return new Packing();
+            case HOST_STRIPING_POLICY:
+                return new Striping();
+            default:
+                throw new LoadingFailedException("Wrong placement policy configuration.");
+        }
+    }
+    
+    @Bean
+    public IStoragePolicy storagePolicy() throws LoadingFailedException {
+        switch(properties.getString("datastorePolicies")) {
+            case DS_PACKING_POLICY:
+                return new StoragePacking();
+            case DS_STRIPING_POLICY:
+                return new StorageStriping();
+            default:
+                throw new LoadingFailedException("Wrong storage policy configuration.");
+        }
+    }
+    
+    @Bean 
+    public IAuthorizationManager authorizationManager() throws LoadingFailedException {
+        if (properties.getBoolean("useXml")) {
+            return new AuthorizationManagerXml(poolConfig.hostPool());
+        } else {
+            return new AuthorizationManager(poolConfig.aclPool(), poolConfig.clusterPool(), poolConfig.hostPool(), poolConfig.datastorePool(), poolConfig.userPool());
+        }        
+    }
+}
